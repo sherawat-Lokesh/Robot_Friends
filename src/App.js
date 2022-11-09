e0738a84d77139ab8efb5757ee83462fe5153bd0
sherawat-Lokesh <107927380+sherawat-Lokesh@users.noreply.github.com>
2022-11-09 08:56:14 +0530

    removed classes and used hooks

--- src/App.js
+++ src/App.js
@@ -1,47 +1,40 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import SearchBar from "./searchBar.js";
 import CardList from "./cardList";
 import Scroll from "./Scroll.js";
 import ErrorBoundry from "./ErrorBoundry";
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      robot: [],
-      searchField: "",
-    };
-  }
-  //if we don't use  arrow function here this.state will not work because it refer to onChange event happening in searchBar file
+const App = () => {
+  const [robot, setRobot] = useState([]);
+  const [searchField, setSearchField] = useState("");
 
-  onSearchInput = (event) => {
-    this.setState({ searchField: event.target.value });
+  //if we don't use  arrow function here this.state will not work because it refer to onChange event happening in searchBar file
+  const onSearchInput = (event) => {
+    setSearchField(event.target.value);
   };
-  componentDidMount() {
+
+  useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
-      .then((json) => this.setState({ robot: json }, console.log(json)));
-  }
+      .then((json) =>{ setRobot(json)});
+  },[]);
 
-  render() {
-    const searchRobot = this.state.robot.filter((val) => {
-      return val.name
-        .toLowerCase()
-        .includes(this.state.searchField.toLowerCase());
-    });
-    return (
-      <div className="tc">
-        <h1 className="f1">ROBOFRIENDS</h1>
-        <SearchBar searchChange={this.onSearchInput} />
-        <Scroll>
-          <ErrorBoundry>
-            <CardList robot={searchRobot} />
-          </ErrorBoundry>
-        </Scroll>
-      </div>
-    );
-  }
-}
+  const searchRobot = robot.filter((val) => {
+    return val.name.toLowerCase().includes(searchField.toLowerCase());
+  });
+
+  return (
+    <div className="tc">
+      <h1 className="f1">ROBOFRIENDS</h1>
+      <SearchBar searchChange={onSearchInput} />
+      <Scroll>
+        <ErrorBoundry>
+          <CardList robot={searchRobot} />
+        </ErrorBoundry>
+      </Scroll>
+    </div>
+  );
+};
 
 export default App;
